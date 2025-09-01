b36d2566e10a9a0d04f164a85c613172854106dc
Jessie Zhong <zhongjessie2003@gmail.com>
2025-08-31 17:25:46 -0700

    add smogon sprites

--- src/components/TraitsComponent.h
+++ src/components/TraitsComponent.h
@@ -6,8 +6,9 @@
 class TraitsComponent : public Component {
  public:
   TraitsComponent(Nature p_nature, Gender p_gender, bool p_isShiny);
-
+  int level;
   Nature nature;
   Gender gender;
   bool isShiny;
+  int friendShip;
 };
